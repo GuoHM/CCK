49e7e4e4f76d1fdb6954a57b11122d8c450fe265
GuoHM <fsguohaoming@hotmail.com>
2020-03-02 20:52:39 +0800

    first 2 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { SacnNRICComponent } from './sacn-nric/sacn-nric.component';
+import { OrderCollectComponent } from './order-collect/order-collect.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { LocationComponent } from './location/location.component';
+import { PaymentComponent } from './payment/payment.component';
+import { InsertCashComponent } from './insert-cash/insert-cash.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { CollectComponent } from './collect/collect.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'scan-NRIC',
+    component: SacnNRICComponent
+  },
+  {
+    path: 'order-collect',
+    component: OrderCollectComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/sacn-nric/sacn-nric.component.ts
+++ src/app/sacn-nric/sacn-nric.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Location} from '@angular/common';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-sacn-nric',
   templateUrl: './sacn-nric.component.html',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SacnNRICComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  forward() {
+    this.router.navigate(['/order-collect']);
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
